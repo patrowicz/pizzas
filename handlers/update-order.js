db039ae609ae7c0956500fc44edecf45f918631e
m.patrowicz <m.patrowicz@pixel.com.pl>
2017-10-29 20:38:40 +0100

    return obj update

--- handlers/update-order.js
+++ handlers/update-order.js
@@ -1,6 +1,8 @@
 function updateOrder(id,order) {
     if (!id || !order || !order.pizzaId || !order.address)
         throw new Error('To order pizza please provide pizza type and address where pizza should be delivered')
-    return {}
+    return {
+        message: `Order ${id} was successfully updated`
+    }
 }
 module.exports = updateOrder
